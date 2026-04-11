82b8d132f3d33cf61e4b2ac543ecb0b9356a306b
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-04-11 23:10:50 +0700

    Database - Membuat Model beserta Migration

---
+++ database/migrations/2026_04_11_215954_create_stock_totals_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_totals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->integer('total_stock')->default(0); // Jumlah stok total yang dapat dipotong oleh transaksi
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_totals');
+    }
+};
---
+++ database/migrations/2026_04_11_224905_create_stock_opname_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_opname_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('stock_opname_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('stock_total_id')->constrained()->cascadeOnDelete();
+            $table->integer('physical_quantity'); // Jumlah fisik yang dihitung selama stock opname
+            $table->integer('quantity_difference'); // Selisih antara jumlah fisik dan jumlah di sistem
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_opname_details');
+    }
+};
