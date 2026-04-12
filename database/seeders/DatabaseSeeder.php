eef0498e7326425eb48f2de515248ae1fb58ce6d
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-04-12 12:00:13 +0700

    Pembuatan Seeder dan Konfigurasi Laravel Permission - Membuat Seeder untuk Permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-       $this->call(RolesTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
     }
 }
