ef12616507831a88583a4f9ea62918995775a9e6
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-04-15 19:31:12 +0700

    Supplier - Membuat Form Request Supplier

---
+++ app/Http/Requests/SupplierRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SupplierRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'phone' => 'required|string|max:20',
+            'description' => 'nullable|string|max:1000',
+            'status' => 'required|in:active,inactive',
+            'province_id' => 'required|exists:provinces,id',
+            'city_id' => 'required|exists:cities,id',
+        ];
+    }
+}
